60da9c913d97e0097ed46f94b9ab2487e7509732
Mateus Lackome <a.mateuslol@gmail.com>
2017-07-01 20:19:23 -0300

    Modificação FIFO - Processo - Entrada - Novas variáveis e métodos adicionados a Processo - Re-implementação FIFO - Nova entrada enviada pela professora

--- src/FIFO/MainFIFO.java
+++ src/FIFO/MainFIFO.java
@@ -9,62 +9,121 @@ import Util.ProcessoSaida;
 public class MainFIFO {
 
 	private ArrayList<ProcessoSaida> ProcessosConcluidos = new ArrayList<>();
-	private int contadorTotal = 0;
+	private ArrayList<Processo> Prontos = Arquivo.getInstance().getListProcessos();
 
 	public void FIFO() {
 
-		ArrayList<Processo> Processos = new ArrayList<>();
-		Processos = Arquivo.getInstance().getListProcessos();
+		this.executando();
 
-		for (byte x = 0; x < Processos.size(); x++) {
+	}
 
-			processar(Processos.get(x));
+	public void pronto(int x) {
+
+		for (Processo p : Prontos) { // conta o tempo de pronto para todos os
+										// processos no array
+			p.setTempoPronto(x);
+		}
+
+	}
+
+	public void encerrado(Processo p) {
+
+		int tPRONTO = p.TempoEstadoPronto(); // tempo total no estado pronto
+		int tIO = p.TempoEstadoBloqueado(); // tempo total IO - bloqueado
+		int tRESPOSTA = p.getStopPoint(); // tempo em que o PC finaliza o
+											// processo
+		int ID = p.getID(); // ID do processo
+
+		ProcessoSaida pSAIDA = new ProcessoSaida(ID, tRESPOSTA, tIO, tPRONTO);
+
+		this.ProcessosConcluidos.add(pSAIDA);
+
+	}
+
+	public void executando() {
+
+		boolean chave = true;
+		boolean fim = false;
+		boolean executando = true;
+		Processo p = new Processo();
+		int PC = 0;
+		int aux0 = 0;
+
+		while (fim == false) {
+
+			if (chave) { // siginifica que tem que pegar processo da fila de
+							// pronto
+
+				if (!Prontos.isEmpty()) { // enquanto prontos não estiver vazio
+					p = Prontos.get(0);
+					Prontos.remove(0); // Processo foi retirado da fila de
+										// pronto e a fila é atualizada
+					chave = false;
+
+				} else { // fim da execução
+					fim = true;
+				}
+
+			}
+			if (!chave) { // fluxo seguinte
+
+				while (executando) {
+
+					if ((p.containsTempoIO(PC) == false) && (p.getTempoComputacao() > 0)) { // executando...
+
+						this.pronto(PC); // conta pronto no array de prontos
+						aux0 = p.getTempoComputacao();
+						p.setTempoComputacao(aux0--); // decrementa tempo de
+														// computação
+						PC++; // conta PC
+
+					} else if (p.containsTempoIO(PC)) { // chamada i.o
+
+						p.setBloqPoint(PC); // o tempo em PC cujo foi encontrado
+											// o bloqueio no processo
+						bloqueado(p); // passa pra bloqueado
+
+					} else if (p.getTempoComputacao() == 0) { // acaba tempo de
+																// computação
+
+						p.setStopPoint(PC); // tempo em que foi concluido
+						encerrado(p); // constrói o processo final e finaliza o
+										// processo
+						executando = false; // faz sair do loop de execução
+						chave = true; // faz solicitar novo processo a fila de
+										// pronto
+
+					}
+
+				}
+
+			}
 
 		}
 
 	}
 
-	public void processar(Processo processoAtual) {
+	public void bloqueado(Processo b) {
 
-		ProcessoSaida pSaida = new ProcessoSaida();
+		int num = b.getBloqPoint(); // pega o ponto de parada do pc
+		ArrayList<Integer> temposIO = b.getTemposIO(); // pega os tempos de i/o
+		boolean key = true;
 
-		int IDprocesso = processoAtual.getID();
-		int tempoComp = processoAtual.getTempoComputacao();
-
-		ArrayList<Integer> temposIO = processoAtual.getTemposIO();
-		// Estados: 0 - bloqueado ; 1 - pronto ; 2 - executando
-		byte Estado = 1;
-
-		int contadorTComp = 0;
-		int contadorIO = 0;
-
-		int contadorPronto = 0;
-
-		while (contadorTComp != tempoComp) {
-
-			if (temposIO.contains(contadorTComp)) {
-
-				Estado = 0;
-				contadorIO++;
-				contadorTComp--;
-
-			} else if ((Estado == 0) && ((temposIO.contains(contadorTComp)) == false)) {
-
-				Estado = 1;
-				contadorPronto++;
-
+		while (key == true) {
+			if (key == true && temposIO.contains(num)) { // enquanto contém o
+															// valor no array
+															// este valor é
+															// removido
+				temposIO.remove(num);
+				num++;
 			} else {
-
-				Estado = 2;
-				contadorTComp++;
-				contadorTotal++;
+				key = false;
 			}
 
 		}
 
-		pSaida = new ProcessoSaida(IDprocesso, contadorTotal, contadorIO, contadorPronto);
-
-		ProcessosConcluidos.add(pSaida);
+		b.setTemposIO(temposIO); // seta o array de temposIO no processo
+		this.Prontos.add(b); // manda o processo para o final da fila de prontos
 
 	}
 
--- src/Util/Processo.java
+++ src/Util/Processo.java
@@ -3,12 +3,13 @@ package Util;
 import java.util.ArrayList;
 
 public class Processo {
-	
+
 	/**
-	 * Classe básica Processo 
-	 * Representando os campos passados nos requisitos do projeto
+	 * Classe básica Processo Representando os campos passados nos requisitos do
+	 * projeto
 	 */
-	
+
+	// variáveis base do processo
 	private int ID;
 	private int TempoChegada;
 	private int TempoComputacao;
@@ -17,10 +18,15 @@ public class Processo {
 	private int Periodo;
 	private int Deadline;
 	
-	public Processo(){
-		
+	//variáveis extras
+	private ArrayList<Integer> TempoPronto; //tempos no estado pronto
+	private int StopPoint; //tempo de finalização do processo no pc
+	private int BloqPoint; //tempo de bloqueio do processo no pc
+
+	public Processo() {
+
 	}
-	
+
 	public Processo(int iD, int tempoChegada, int tempoComputacao, ArrayList<Integer> temposIO, int prioridade,
 			int periodo, int deadline) {
 		ID = iD;
@@ -31,52 +37,97 @@ public class Processo {
 		Periodo = periodo;
 		Deadline = deadline;
 	}
-	
-	
+
 	public int getID() {
 		return ID;
 	}
+
 	public void setID(int iD) {
 		ID = iD;
 	}
+
 	public int getTempoChegada() {
 		return TempoChegada;
 	}
+
 	public void setTempoChegada(int tempoChegada) {
 		TempoChegada = tempoChegada;
 	}
+
 	public int getTempoComputacao() {
 		return TempoComputacao;
 	}
+
 	public void setTempoComputacao(int tempoComputacao) {
 		TempoComputacao = tempoComputacao;
 	}
+
 	public ArrayList<Integer> getTemposIO() {
 		return TemposIO;
 	}
+	
+	public boolean containsTempoIO(int x){
+		return this.TemposIO.contains(x);
+	}
+
 	public void setTemposIO(ArrayList<Integer> temposIO) {
 		TemposIO = temposIO;
 	}
+
 	public int getPrioridade() {
 		return Prioridade;
 	}
+
 	public void setPrioridade(int prioridade) {
 		Prioridade = prioridade;
 	}
+
 	public int getPeriodo() {
 		return Periodo;
 	}
+
 	public void setPeriodo(int periodo) {
 		Periodo = periodo;
 	}
+
 	public int getDeadline() {
 		return Deadline;
 	}
+
 	public void setDeadline(int deadline) {
 		Deadline = deadline;
 	}
+
+	public int getTempoPronto() {
+		return TempoPronto.size();
+	}
+
+	public void setTempoPronto(int i) {
+		this.TempoPronto.add(i);
+	}
+
+	public int getStopPoint() {
+		return StopPoint;
+	}
+
+	public void setStopPoint(int stopPoint) {
+		StopPoint = stopPoint;
+	}
+
+	public int getBloqPoint() {
+		return BloqPoint;
+	}
+
+	public void setBloqPoint(int bloqPoint) {
+		BloqPoint = bloqPoint;
+	}
 	
+	public int TempoEstadoPronto(){
+		return TempoPronto.size();
+	}
 	
-	
+	public int TempoEstadoBloqueado(){
+		return TemposIO.size();
+	}
 
 }
